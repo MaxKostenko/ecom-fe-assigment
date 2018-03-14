f0fb4b301e33039a49cb50916a6c8849a9e4132f
Max Kostenko <m.u.kostenko@gmail.com>
2018-03-14 21:05:09 +0100

    Slim framework was installed

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+require '../vendor/autoload.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Hello, first");
+
+    return $response;
+});
+
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
