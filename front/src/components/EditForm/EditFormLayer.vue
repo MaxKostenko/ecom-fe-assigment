1877a216ec28332f4b21e07f427c3eb6fe3ee568
Max Kostenko <m.u.kostenko@gmail.com>
2018-03-15 23:17:34 +0100

    Component Layer was devided

---
+++ front/src/components/EditForm/EditFormLayer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="holder">
+    <h3>New Blog Post</h3>
+    <div class="form">
+        <div class="form-fields">
+          <slot></slot>
+        </div>
+        <div class="form-action">
+          <slot name="action"></slot>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .holder {
+    border: 1px solid #444;
+    border-radius: 10px;
+    margin: 20px;
+    padding: 20px;
+  }
+
+  .form {
+    margin: 40px 0 40px 0;
+    display: inline-block;
+    width: 100%;
+  }
+
+  .form-action::after {
+    clear: both;
+  }
+
+  .form-fields {
+    float: left;
+    width: 75%;
+  }
+
+  .form-action {
+    float: left;
+    width: 25%;
+    padding: 0 10px 0 40px;
+  }
+
+</style>
---
+++ front/src/components/EditForm/SubmitButton.vue
@@ -0,0 +1,15 @@
+<template functional>
+  <button type="submit">Send Form</button>
+</template>
+
+<style>
+  .form-action button {
+    height: 45px;
+    width: 100%;
+    background-color: cornflowerblue;
+    color: white;
+    border: 0;
+    border-radius: 3px;
+    font-size: larger;
+  }
+</style>
